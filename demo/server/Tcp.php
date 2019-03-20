1f27c42d4bdda0ab87f8e5a8c0f85e01c262a32f
Houguanglong <369380397@qq.com>
2019-03-20 23:32:29 +0800

    提交

--- demo/server/Tcp.php
+++ demo/server/Tcp.php
@@ -8,13 +8,22 @@
 //创建Server对象，监听 127.0.0.1:9501端口
 $serv = new swoole_server("127.0.0.1", 9501);
 
+$serv->set([
+    'worker_num'=>4,
+    'max_request'=>50
+]);
+
+/**
+ * @param int $fd  客户端链接用户的唯一标识
+ * @param int $reactor_id 线程id号
+ */
 //监听连接进入事件
-$serv->on('connect', function ($serv, $fd) {
-    echo "Client: Connect.\n";
+$serv->on('connect', function ($serv, $fd,$reactor_id) {
+    echo "Client: {$fd}-{$reactor_id}-Connect.\n";
 });
 
 //监听数据接收事件
-$serv->on('receive', function ($serv, $fd, $from_id, $data) {
+$serv->on('receive', function ($serv, $fd, $reactor_id, $data) {
     $serv->send($fd, "Server: ".$data);
 });
 
