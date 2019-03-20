e173c9b362e419a63ffb1fc3b4680ee58602c222
Houguanglong <369380397@qq.com>
2019-03-20 23:11:18 +0800

    提交

---
+++ demo/server/Tcp.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * User: 侯光龙
+ * FileName: 文件名称
+ * Date: 2019/3/20
+ * Time: 23:09
+ */
+//创建Server对象，监听 127.0.0.1:9501端口
+$serv = new swoole_server("127.0.0.1", 9501);
+
+//监听连接进入事件
+$serv->on('connect', function ($serv, $fd) {
+    echo "Client: Connect.\n";
+});
+
+//监听数据接收事件
+$serv->on('receive', function ($serv, $fd, $from_id, $data) {
+    $serv->send($fd, "Server: ".$data);
+});
+
+//监听连接关闭事件
+$serv->on('close', function ($serv, $fd) {
+    echo "Client: Close.\n";
+});
+
+//启动服务器
+$serv->start();
