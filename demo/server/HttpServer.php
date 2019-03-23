72c84bd5a6fb6cb956f5b862e71267410c24c4b3
Houguanglong <369380397@qq.com>
2019-03-23 14:59:14 +0800

    提交

--- demo/server/HttpServer.php
+++ demo/server/HttpServer.php
@@ -9,8 +9,8 @@ use Swoole\Http\Server;
 //创建http_server服务器 监听127.0.0.1:8811端口
 $http = new Server('0.0.0.0',8811);
 $http->on('request',function ($request,$response){
-    $request->cookie('houguang','very good',120);
     print_r($request->get);
+    $response->cookie('houguang','very good',120);
     $response->end("<h1>Http-server</h1>".$request->get['age']);
 });
 
