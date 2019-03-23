2cfcff44800f53d6a029e99a937bbcd3455b028c
Houguanglong <369380397@qq.com>
2019-03-23 14:09:41 +0800

    提交

---
+++ demo/server/HttpServer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * User: 侯光龙
+ * FileName: 文件名称
+ * Date: 2019/3/23
+ * Time: 13:54
+ */
+use Swoole\Http\Server;
+//创建http_server服务器 监听127.0.0.1:8811端口
+$http = new Server('127.0.0.1',8811);
+$http->on('request',function ($request,$response){
+    $response->end("<h1>Http-server</h1>");
+});
+
+$http->start();
